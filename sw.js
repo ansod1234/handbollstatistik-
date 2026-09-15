e9756527c0747ed3543840d2f8cace61d0da55c8
ansod1234 <andreas.soderberg76@gmail.com>
2026-09-15 14:21:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'handboll-stat-v2.5.1';
+const CACHE_NAME = 'handboll-stat-v3.0';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
